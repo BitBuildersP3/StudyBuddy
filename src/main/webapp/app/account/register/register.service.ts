d1523caf9311d29cb5397c12cc7e6ae42ffb2962
Hijo De Algo <tomatejuan@gmail.com>
2023-03-25 01:09:34 -0600

    Servicio (Aun no es funcional con todo pero si funciona normal con lo que habia antes)

--- src/main/webapp/app/account/register/register.service.ts
+++ src/main/webapp/app/account/register/register.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {from, Observable} from 'rxjs';
 
 import { ApplicationConfigService } from 'app/core/config/application-config.service';
 import { Registration } from './register.model';
+
+
+// import { ExtraUserInfoService } from '../service/extra-user-info.service';
+// Importo la interface de extra-user-info.model.ts
+import { IExtraUserInfo } from 'app/entities/extra-user-info/extra-user-info.model';
 
 @Injectable({ providedIn: 'root' })
 export class RegisterService {
@@ -14,6 +19,10 @@ export class RegisterService {
     console.log("Objeto registration: " + registration);
    const userRegistration = this.extractUser(registration);
     console.log("Objeto registration: " + userRegistration);
+
+    // Make me a get request that returns me the id of the user that I just created.
+
+    // this.http.get(this.applicationConfigService.getEndpointFor('api/register'), userRegistration)
     return this.http.post(this.applicationConfigService.getEndpointFor('api/register'), userRegistration);
   }
 
@@ -23,4 +32,28 @@ export class RegisterService {
     const { login, email, password, langKey } = registration;
     return { login: login, email: email, password: password, langKey: langKey };
   }
+/*
+  extractExtraUserInfo(registration: Registration): object {
+
+    this.http.get(this.applicationConfigService.getEndpointFor('api/register'), registration);
+
+
+
+    const phone = registration.phone;
+    const degree = registration.degree;
+    const birthDay = registration.birthDay;
+
+    return { newExtraInfo };
+  } */
 }
+
+/*
+  Id: number;
+  phone?: string | null;
+  degree?: string | null;
+  profilePicture?: string | null;
+  birthDay?: dayjs.Dayjs | null;
+  score?: number | null;
+  userVotes?: number | null;
+  user?: Pick<IUser, 'id' | 'login'> | null;
+ */
