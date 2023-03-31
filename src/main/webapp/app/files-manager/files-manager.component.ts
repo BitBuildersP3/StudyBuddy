66fce523d401ad6a8297f36f4c610ebf0a364780
Hijo De Algo <tomatejuan@gmail.com>
2023-03-31 15:55:08 -0600

    Componente Basico Dinamico funcional

--- src/main/webapp/app/files-manager/files-manager.component.ts
+++ src/main/webapp/app/files-manager/files-manager.component.ts
@@ -5,7 +5,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./files-manager.component.scss'],
 })
 export class FilesManagerComponent implements OnInit {
+
+  selectedFileComponent: string = 'video';
   constructor() {}
 
   ngOnInit(): void {}
+
+  showVideoFile() {
+    this.selectedFileComponent = 'video';
+  }
+
+  showIndexedFile() {
+    this.selectedFileComponent = 'indexed';
+  }
+
+  showOwnFile() {
+    this.selectedFileComponent = 'own';
+  }
 }
