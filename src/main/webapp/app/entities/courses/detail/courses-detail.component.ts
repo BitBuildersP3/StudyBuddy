05cad198e732b7e7c4e7e194c06ed2280d61d37b
Andrey Meléndez <68828555+4ndreyMS@users.noreply.github.com>
2023-04-01 17:52:57 -0600

    Feature of show course (#25)
    * working on the listing of the clases and showing the video
    
    * adding data and styles
    
    * adding syles and btns
    
    * adding syles and btns
    
    * renderin from api and adding syles to the active
    
    * renderin from api and adding syles to the active
    
    ---------
    
    Co-authored-by: AndreyDotcms <andrey.melendez@dotcms.com>

--- src/main/webapp/app/entities/courses/detail/courses-detail.component.ts
+++ src/main/webapp/app/entities/courses/detail/courses-detail.component.ts
@@ -1,21 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
 import { ICourses } from '../courses.model';
-
+import { CoursesService } from '../service/courses.service';
 @Component({
   selector: 'jhi-courses-detail',
   templateUrl: './courses-detail.component.html',
+  styleUrls: ['./courses-detail.component.scss'],
 })
 export class CoursesDetailComponent implements OnInit {
-  courses: ICourses | null = null;
+  courses: any;
 
-  constructor(protected activatedRoute: ActivatedRoute) {}
+  linkTitle = 'Abrir';
+
+  @ViewChild('thenBlock')
+  thenBlock: any = '';
+
+  @ViewChild('elseBlock')
+  elseBlock: any = '';
+
+  currentSection: any = {};
+
+  courseResponse: any = {};
+  sections: any = ['init'];
+
+  isActive: any = 0;
+  constructor(protected activatedRoute: ActivatedRoute, private courseService: CoursesService) {}
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ courses }) => {
       this.courses = courses;
     });
+    this.courseService.getCouseDataById(this.courses?.id).subscribe(data => {
+      this.courseResponse = data.body;
+      this.sections = this.courseResponse.sections;
+      this.setCurrentSection(this.sections[0]);
+    });
+  }
+
+  setActive(index: any): void {
+    this.isActive = index;
+  }
+
+  setCurrentSection(section: any): void {
+    this.currentSection = section;
+
+    //
   }
 
   previousState(): void {
