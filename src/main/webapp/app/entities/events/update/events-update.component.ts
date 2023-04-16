6bd46bfaa946f78a7badf73f42076172e353f39b
liz0921 <lmonteror@ucenfotec.ac.cr>
2023-04-16 17:15:45 -0600

    set autor

--- src/main/webapp/app/entities/events/update/events-update.component.ts
+++ src/main/webapp/app/entities/events/update/events-update.component.ts
@@ -3,13 +3,14 @@ import { HttpResponse } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { finalize, map } from 'rxjs/operators';
-
+import { ExtraUserInfoService } from 'app/entities/extra-user-info/service/extra-user-info.service';
+import { EntityResponseType } from 'app/entities/news/service/news.service';
 import { EventsFormService, EventsFormGroup } from './events-form.service';
 import { IEvents } from '../events.model';
 import { EventsService } from '../service/events.service';
 import { IUser } from 'app/entities/user/user.model';
 import { UserService } from 'app/entities/user/user.service';
-import {User} from "../../../admin/user-management/user-management.model";
+import { User } from '../../../admin/user-management/user-management.model';
 
 @Component({
   selector: 'jhi-events-update',
@@ -19,10 +20,13 @@ import {User} from "../../../admin/user-management/user-management.model";
 export class EventsUpdateComponent implements OnInit {
   isSaving = false;
   events: IEvents | null = null;
+  idUser: number = 0;
+  status = '';
 
   usersSharedCollection: IUser[] = [];
 
   editForm: EventsFormGroup = this.eventsFormService.createEventsFormGroup();
+  ownerName: string = '';
 
   user: User | null = null;
 
@@ -31,32 +35,29 @@ export class EventsUpdateComponent implements OnInit {
     protected eventsFormService: EventsFormService,
     protected userService: UserService,
     protected activatedRoute: ActivatedRoute,
-
-  private route: ActivatedRoute
+    protected extraUser: ExtraUserInfoService,
+    private route: ActivatedRoute
   ) {}
 
   compareUser = (o1: IUser | null, o2: IUser | null): boolean => this.userService.compareUser(o1, o2);
 
   ngOnInit(): void {
-    this.activatedRoute.data.subscribe(({ events }) => {
-      this.events = events;
-
-      // Para obtener el usuario autor
-      this.route.data.subscribe(({ user }) => {
-        this.user = user;
-        console.log('ESTE USUARIO ES: +', this.user?.login)
-      });
-
-      if (events) {
-        this.updateForm(events);
-      }
-
-      this.loadRelationshipsOptions();
+    this.extraUser.getInfoByCurrentUser().subscribe({
+      next: (res: EntityResponseType) => {
+        // @ts-ignore
+        this.idUser = res.body?.user.id;
+        const login = res.body?.user?.login;
+        if (login != null) {
+          this.ownerName = login;
+          console.log(this.ownerName);
+        }
+      },
     });
 
-
-
-
+    this.activatedRoute.data.subscribe(({ events }) => {
+      this.events = events;
+      //this.loadRelationshipsOptions();
+    });
   }
 
   previousState(): void {
@@ -64,11 +65,15 @@ export class EventsUpdateComponent implements OnInit {
   }
 
   save(): void {
+    const user: Pick<IUser, 'id' | 'login'> = { id: this.idUser, login: this.ownerName };
+
     this.isSaving = true;
     const events = this.eventsFormService.getEvents(this.editForm);
     if (events.id !== null) {
       this.subscribeToSaveResponse(this.eventsService.update(events));
     } else {
+      events.status = 'pendiente';
+      events.user = user;
       this.subscribeToSaveResponse(this.eventsService.create(events));
     }
   }
